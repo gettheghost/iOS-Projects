db108bd1072dedbc799e49fdcbb495270db22103
Jasey <53540302+jaseytech@users.noreply.github.com>
2020-01-03 19:18:46 -0500

    Update NewClaimViewController+Contacts.swift

--- Redwoods-Project/New Claim/NewClaimViewController+Contacts.swift
+++ Redwoods-Project/New Claim/NewClaimViewController+Contacts.swift
@@ -1,11 +1,3 @@
-//
-//  NewClaimViewController+Contacts.swift
-//  Redwoods Project
-//
-//  Created by Kevin Poorman on 11/29/18.
-//  Copyright © 2018 Salesforce. All rights reserved.
-//
-
 import UIKit
 import Contacts
 import ContactsUI
